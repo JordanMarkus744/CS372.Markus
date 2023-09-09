800df863591a42905d78658c8768e0614404963a
Jordan Markus <54387896+JordanMarkus744@users.noreply.github.com>
2023-09-09 15:40:01 -0500

    Update Asg4P1.cpp

--- apps/Assignment 4/Asg4P1.cpp
+++ apps/Assignment 4/Asg4P1.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "HotPotato.hpp"
 
-int main(){
+int main(){ // I used a doubly Circular Linked List for the base of this game
     HotPotato game;
-    game.PlayGame(5,0);
+    game.PlayGame(5,0); // First integer input is the amount of players, second integer input is how many tosses until someone is out
     return 0;
 }
