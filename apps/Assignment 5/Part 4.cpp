36b658c50abbc0c9514c4f29ffc106a18579a09d
Jordan Markus <54387896+JordanMarkus744@users.noreply.github.com>
2023-10-02 04:23:28 -0500

    updates

---
+++ apps/Assignment 5/Part 4.cpp
@@ -0,0 +1,45 @@
+#include "Tree.hpp"
+#include <iostream>
+#include <memory>
+
+std::shared_ptr<Tree> createTreeWithoutLeaves(const Tree& originalTree) {
+    std::shared_ptr<Tree> newTree = std::make_shared<Tree>();
+
+        // Helper function to copy the tree without leaves
+    std::shared_ptr<Node> copyWithoutLeaves(const std::shared_ptr<Node>& currentNode) {
+        if (!currentNode) {
+            return nullptr;
+        }
+
+        if (!currentNode->left && !currentNode->right) {
+            return nullptr; // Omit leaves
+        }
+
+        std::shared_ptr<Node> newNode = std::make_shared<Node>(currentNode->data);
+        newNode->left = copyWithoutLeaves(currentNode->left);
+        newNode->right = copyWithoutLeaves(currentNode->right);
+
+        return newNode;
+    }
+
+    newTree->root = copyWithoutLeaves(originalTree.root);
+    return newTree;
+}
+
+
+int main() {
+    // Example usage
+    Tree<int> originalTree;
+    originalTree.addLeaf(10);
+    originalTree.addLeaf(5);
+    originalTree.addLeaf(15);
+    originalTree.addLeaf(3);
+    originalTree.addLeaf(7);
+    originalTree.addLeaf(12);
+    originalTree.addLeaf(18);
+
+    std::shared_ptr<Tree<int>> newTree = createTreeWithoutLeaves(originalTree);
+    newTree->Traverse(); // Traverse the new tree
+
+    return 0;
+}
