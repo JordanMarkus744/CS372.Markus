2b163daa4cc7fa77c314207c2085c3eb5e5d86db
Jordan Markus <54387896+JordanMarkus744@users.noreply.github.com>
2023-10-02 02:44:05 -0500

    DeleteLeaf function added

--- include/Tree.hpp
+++ include/Tree.hpp
@@ -1,17 +1,18 @@
-#include <memory.h>
+#include <memory>
 #pragma once
+// Got code from Dr. Lewis Lecture Notes
 template<typename V>
 class Tree {
 private:
    struct Node {
-      Node(V v, shared_ptr<Node> & l, shared_ptr<Node>Node & r):
+      Node(V v, std::shared_ptr<Node> & l, std::shared_ptr<Node>Node & r):
                value(v), left(l), right(r) {}
       V value;
-      shared_ptr<Node> left;
-      shared_ptr<Node> right;
+      std::shared_ptr<Node> left;
+      std::shared_ptr<Node> right;
    };
    explicit Tree(shared_ptr<Node> & node): root(node) {}
-   shared_ptr<Node> root;
+   std::shared_ptr<Node> root;
 public:
    Tree() {}
    Tree(Tree const &lft, V value, Tree const &rgt) : root(Node(val, lft->root, rgt->root )) {}
@@ -20,6 +21,31 @@ public:
    V getRoot() const { return root->val; }
    Tree &left() const {return root->left; }
    Tree &right() const {return root->right; }
+
+
+    bool deleteLeaf(V x) {
+    return deleteLeaf(root, x);
+}
+
+bool deleteLeaf(std::shared_ptr<Node>& currentNode, V x) {
+    if (!currentNode) {
+        return false; // Node not found
+    }
+
+    if (currentNode->value == x) {
+        // Node with value x found, delete it
+        currentNode = nullptr;
+        return true;
+    }
+
+    // Recursively search in the left and right subtrees
+    bool deletedLeft = deleteLeaf(currentNode->left, x);
+    bool deletedRight = deleteLeaf(currentNode->right, x);
+
+    return deletedLeft || deletedRight;
+
+}
+
 
    bool member(V x) const{
     if (isEmpty()){
