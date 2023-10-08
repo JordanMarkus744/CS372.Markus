3fb511ffdbe31e6d9d2fa485816cf8e53c81085d
Jordan Markus <54387896+JordanMarkus744@users.noreply.github.com>
2023-10-07 22:00:04 -0500

    Create Part 2 - Jordan Markus.cpp

---
+++ apps/Assignment 6/Part 2 - Jordan Markus.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <random>
+#include <time.h>
+#include "Tree.hpp"
+
+struct MyStructure {
+    size_t size;
+    std::unique_ptr<std::vector<int>> intVector;
+    std::shared_ptr<Tree<int>> binarySearchTree;
+};
+
+// Function to create and initialize the structure
+std::unique_ptr<MyStructure> createAndInitializeStructure(size_t numItems) {
+    auto myStruct = std::make_unique<MyStructure>();
+    myStruct->size = numItems;
+    myStruct->intVector = std::make_unique<std::vector<int>>();
+    myStruct->binarySearchTree = std::make_shared<Tree<int>>(); // Assuming you have a Tree class defined
+
+    // Fill the vector with random integers
+    std::srand(time(NULL));
+
+    for (size_t i = 0; i < numItems; ++i) {
+        int randomValue = rand() % 1000;
+        myStruct->intVector->push_back(randomValue);
+    }
+
+    // Sort the vector
+    std::sort(myStruct->intVector->begin(), myStruct->intVector->end());
+
+    // Insert sorted integers into the binary search tree
+    for (int value : *(myStruct->intVector)) {
+        myStruct->binarySearchTree->addLeaf(value);
+    }
+
+    return myStruct;
+}
+
+int main() {
+    size_t numItems = 100; // Change this as needed
+    auto myStruct = createAndInitializeStructure(numItems);
+
+    myStruct->binarySearchTree->Traverse();
+
+    return 0;
+}
+
